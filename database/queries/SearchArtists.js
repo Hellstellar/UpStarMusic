71c2bc1e65f165ee3c5910b9ae716ade891975b6
Sooraj Dube <soorajdube.is@gmail.com>
2018-08-03 22:35:58 +0530

    done project

--- database/queries/SearchArtists.js
+++ database/queries/SearchArtists.js
@@ -8,9 +8,28 @@ const Artist = require('../models/artist');
  * @param {integer} limit How many records to return in the result set
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
+
+const buildQuery = (criteria) => {
+    const findQuery = {}
+    if(criteria.name.length !== 0)
+        findQuery.$text = { $search: criteria.name }
+        
+    if(criteria.age) {
+        findQuery.age = { $gte: criteria.age.min, $lte: criteria.age.max };
+    }
+
+    if(criteria.yearsActive) {
+        findQuery.yearsActive = { $gte: criteria.yearsActive.min, $lte: criteria.yearsActive.max };
+    }
+
+    return findQuery;
+}
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
-    const query = Artist.find({}).sort({ [sortProperty]: 1 }).skip(offset).limit(20)
-    const count = Artist.countDocuments()
+        
+    const query = Artist.find(buildQuery(criteria))
+                        .sort({ [sortProperty]: 1 })
+                        .skip(offset).limit(20);
+    const count = Artist.find(buildQuery(criteria)).count();
 
     return Promise.all([query, count])
                     .then(values => {
@@ -19,6 +38,6 @@ module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
                             count: values[1],
                             offset,
                             limit
-                        }
+                        };
                     });
-}
+};
